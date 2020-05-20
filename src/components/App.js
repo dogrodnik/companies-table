f4cad02e59317084482c8ae88ff728180649d611
dogrodnik <d.ogrodnik96@gmail.com>
2020-05-20 09:59:35 +0200

    Components refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { useCompaniesFetch } from "../custom-hooks/useCompaniesFetch";
+
+import { Pagination } from "./Pagination";
+import { Search } from "./Search";
 import { Table } from "./Table";
 
 function App() {
+  const [companiesArray, setCompaniesArray, error] = useCompaniesFetch();
+  const [companiesToDisplay, setCompaniesToDisplay] = useState([]);
+  const [pages, setPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const ROWS_ON_PAGE = 15;
+
+  useEffect(() => {
+    if (companiesArray) {
+      setPages(Math.ceil(companiesArray.length / ROWS_ON_PAGE));
+      const companieSlice = companiesArray.slice(
+        currentPage * ROWS_ON_PAGE,
+        currentPage * ROWS_ON_PAGE + ROWS_ON_PAGE
+      );
+      setCompaniesToDisplay(companieSlice);
+    }
+  }, [companiesArray, currentPage]);
+
   return (
     <div className="App">
-      <Table />
+      <Search
+        companiesArray={companiesArray}
+        setCompaniesArray={setCompaniesArray}
+        setCurrentPage={setCurrentPage}
+      />
+      <Table
+        companiesArray={companiesArray}
+        setCompaniesArray={setCompaniesArray}
+        companiesToDisplay={companiesToDisplay}
+      />
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pages={pages}
+      />
     </div>
   );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,54 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { Content } from "./Content";
 import { Header } from "./Header";
-import { Pagination } from "../Pagination";
-import { Search } from "../Search";
+import { Content } from "./Content";
 
-import { useCompaniesFetch } from "../../custom-hooks/useCompaniesFetch";
-
-export const Table = () => {
-  const [companiesArray, setCompaniesArray, error] = useCompaniesFetch();
-  const [companiesToDisplay, setCompaniesToDisplay] = useState([]);
-  const [pages, setPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(0);
-  const COMPANIES_ON_PAGE = 15;
-
-  useEffect(() => {
-    if (companiesArray) {
-      setPages(Math.ceil(companiesArray.length / COMPANIES_ON_PAGE));
-      const companieSlice = companiesArray.slice(
-        currentPage * COMPANIES_ON_PAGE,
-        currentPage * COMPANIES_ON_PAGE + COMPANIES_ON_PAGE
-      );
-      setCompaniesToDisplay(companieSlice);
-    }
-  }, [companiesArray, currentPage]);
-
+export const Table = ({
+  companiesArray,
+  setCompaniesArray,
+  companiesToDisplay,
+}) => {
   return (
     companiesArray && (
-      <>
-        <Search
-          companiesArray={companiesArray}
-          setCompaniesArray={setCompaniesArray}
-          setCurrentPage={setCurrentPage}
-        />
-        <div className="table__wrapper">
-          <table>
-            <Header
-              companiesArray={companiesArray}
-              setCompaniesArray={setCompaniesArray}
-            />
-            <Content companies={companiesToDisplay} />
-          </table>
-        </div>
-
-        <Pagination
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-          pages={pages}
-        />
-      </>
+      <div className="table__wrapper">
+        <table>
+          <Header
+            companiesArray={companiesArray}
+            setCompaniesArray={setCompaniesArray}
+          />
+          <Content companies={companiesToDisplay} />
+        </table>
+      </div>
     )
   );
 };
