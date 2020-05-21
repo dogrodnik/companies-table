b4255223bcb09b4f26a7919f6e9b2846759f47ab
dogrodnik <d.ogrodnik96@gmail.com>
2020-05-21 10:44:46 +0200

    Updates

---
+++ precache-manifest.a129b35f372ac98df24bc2a7dcd8a1c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb30bc015c97c20e63357bd673c1cd8",
+    "url": "/companies-table/index.html"
+  },
+  {
+    "revision": "51d5bd6325739a0746c5",
+    "url": "/companies-table/static/css/main.32453eef.chunk.css"
+  },
+  {
+    "revision": "8157f1f89ed3fb19901a",
+    "url": "/companies-table/static/js/2.4c4aeef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/companies-table/static/js/2.4c4aeef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d5bd6325739a0746c5",
+    "url": "/companies-table/static/js/main.1b3480ae.chunk.js"
+  },
+  {
+    "revision": "ed89491fa9e04a01c543",
+    "url": "/companies-table/static/js/runtime-main.c80ea913.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/companies-table/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/companies-table/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "d29afa2d6dc39c9d028b373fffbe82f9",
+    "url": "/companies-table/static/media/loupe.d29afa2d.png"
+  }
+]);
